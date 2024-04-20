b8705f532c62e331fdd09b28958ae27dd5d0457c
RSH <fbtnghks7906@gmail.com>
2024-04-20 21:24:25 +0900

    타겟 숫자에 해당하는 인덱스 반환

---
+++ src/entry/loops/FindIndex.java
@@ -0,0 +1,12 @@
+package entry.loops;
+
+public class FindIndex {
+    public int myFindIndex(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
